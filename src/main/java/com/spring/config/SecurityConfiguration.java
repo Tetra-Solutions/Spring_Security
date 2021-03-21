4db72d5e2c04e5b1a4387a4a56f4c7d58ddd2db8
dejvis06 <dejvisbeqiraj06@gmail.com>
2021-03-21 17:13:24 +0100

    imports

--- src/main/java/com/spring/config/SecurityConfiguration.java
+++ src/main/java/com/spring/config/SecurityConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import com.spring.enumeration.Role;
 import com.spring.filter.JwtAccessDeniedHandler;
 import com.spring.filter.JwtAuthenticationEntryPoint;
 import com.spring.filter.JwtAuthorizationFilter;
